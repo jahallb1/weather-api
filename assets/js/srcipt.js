901ec876fec75a2ab2e6bbbba3de60c5ad6239e1
Joshua Hall <joshuahall@Joshuas-MacBook-Pro.local>
2020-07-12 20:02:34 -0400

    add forecast code

--- assets/js/srcipt.js
+++ assets/js/srcipt.js
@@ -2,8 +2,9 @@
 const apiAddress = "https://api.openweathermap.org/data/2.5/forecast?q="
 const apiKey = "&appid=95383217fb8a071176b4486374305aee";
 const apiUnits = "&units=imperial";
-let cityNameEl = document.querySelector("#city-name");
+let cityNameEl = document.querySelector("#city");
 let containerEl = document.querySelector(".container");
+let forcastContainerEl = document.querySelector(".container-forecast");
 
 
 $("#btn-primary").on("click", function () {
@@ -28,7 +29,7 @@ function getWeather() {
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    oneApi(data.city.coord.lat, data.city.coord.lon);
+                    oneApi(data.city.coord.lat, data.city.coord.lon, city);
                 });
             } else {
                 alert("Error ")
@@ -37,13 +38,14 @@ function getWeather() {
 
 };
 
-function oneApi(lat, lon) {
+function oneApi(lat, lon, city) {
     let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + apiKey + apiUnits;
     fetch(url)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    displayWeather(data, data.city);
+                    displayWeather(data, city);
+                    console.log(city);
                 });
             } else {
                 alert("Error ")
@@ -52,15 +54,15 @@ function oneApi(lat, lon) {
 }
 
 function displayWeather(data, city) {
-    let cityName = data.city;
     let temp = data.current.temp;
     let humid = data.current.humidity;
     let wind = data.current.wind_speed;
     let uvi = data.current.uvi;
 
-    let cityNameEl = document.createElement("span");
-    cityNameEl.textContent = cityName;
-    containerEl.appendChild(cityNameEl);
+    let cityEl = document.createElement("span");
+    cityEl.textContent = " " + city;
+    console.log(city);
+    cityNameEl.appendChild(cityEl);
 
     let tempEl = document.createElement("p");
     tempEl.textContent = "Temperature " + temp;
@@ -81,12 +83,42 @@ function displayWeather(data, city) {
 
 
 }
-function displayForecast() {
-    
+ function displayForecast(data,) {
+     for (var i = 1; i < 6; i++) {
+         let temp = data.list[i].main.temp;
+         let date = (moment().add(i, "days").format("MM/DD//YYYY"));
+         let icon = list[i].weather[0].icon
+         let humid = data.list[i].main.humidity;
 
-}
+         let tempEl = document.createElement("p");
+         tempEl.textContent = "Temp: " + temp;
+         tempEl.addClass = "card card-body card-text"
+         forcastContainerEl.appendChild(tempEl);
 
+         let dateEl = document.createElement();
+         dateEl.textContent = "<h3>" + date + "</h3>";
+         dateEl.addClass = "card card-title";
+         forcastContainerEl.appendChild(dateEl);
 
+         let iconEl = document.createElement("img");
+         iconEl.textContent = "<img " + "src=" +"http://openweathermap.org/img/wn/" + icon +".png";
+         iconEl.addClass = "card card-body card-text"
+         forcastContainerEl.appendChild(iconEl)
+
+         let humidEl = document.createElement("");
+         humidEl.textContent = "Humidity: " + humid;
+         humidEl.addClass = "card card-body card-text";
+         forcastContainerEl.appendChild(humidEl);
+     }
+         
+         // add in rest of ids
+         //add the elements
+
+ }
+
+ //
+
+displayForecast();
 
 
 // }
